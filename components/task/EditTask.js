2c8e337c06897179ef86b123c3d933271fb6ea69
Adithya Kashyap Pulipaka <adithya2205@gmail.com>
2022-08-31 00:45:19 -0400

    migrate-to-tailwind

--- components/task/EditTask.js
+++ components/task/EditTask.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
-import Button from "../core/Button";
-import Input from "../core/Input";
-import styles from "../../styles/task/EditTask.module.css";
+// import Button from "../core/Button";
+// import Input from "../core/Input";
+// import styles from "../../styles/task/EditTask.module.css";
 
 const EditTask = ({ item, onSave, onCancel }) => {
   const [name, setName] = useState(item.name);
@@ -22,61 +22,45 @@ const EditTask = ({ item, onSave, onCancel }) => {
   return (
     <>
       <div>
-        <h3>Edit Task</h3>
-        <div className={styles.editTask}>
-          <div className={styles.taskHeaders}>
+        <h2 className="text-secondary text-center">Edit Task</h2>
+        <div className="flex justify-center items-center gap-8">
+          <div className="flex flex-col justify-center items-center gap-6 text-secondary">
             <label htmlFor="name">Task Info </label>
             <label htmlFor="completed">Completed</label>
           </div>
-          <div className={styles.taskHeaders}>
-            <Input
+          <div className="flex flex-col justify-center items-center gap-6">
+            <input
+              type="text"
               name="name"
-              id="name"
+              id="input"
               value={name}
               onChange={(e) => setName(e.target.value)}
               ref={nameRef}
+              className="p-2 my-0 mx-2 rounded-[7.5px] border border-solid border-black text-sm"
             />
-            <Input
+            <input
               type="checkbox"
               name="completed"
               id="completed"
               checked={completed}
               onChange={(e) => setCompleted(!completed)}
-              className={styles.checkbox}
+              className="p-2 my-0 mx-2 rounded-[7.5px] border border-solid text-sm mr-auto mb-1"
             />
-            {/* <input
-              type="checkbox"
-              name="completed"
-              id="completed"
-              checked={completed}
-              onChange={(e) => setCompleted(e.target.value)}
-            /> */}
           </div>
         </div>
-
-        {/* <div className={styles.rowItem}>
-          <Input
-            name="name"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            ref={nameRef}
-          />
-        </div>
-        <div className={styles.rowItem}>
-          <input
-            type="checkbox"
-            name="completed"
-            id="completed"
-            checked={completed}
-            onChange={(e) => setCompleted(e.target.value)}
-          />
-        </div> */}
-        <div>
-          <Button secondary onClick={saveData}>
+        <div className="text-center">
+          <button
+            className="text-primary bg-blue-600 p-2 m-2 border-none cursor-pointer rounded-[7.5px] text-sm"
+            onClick={saveData}
+          >
             Save
-          </Button>
-          <Button onClick={() => onCancel()}>Cancel</Button>
+          </button>
+          <button
+            className="text-primary bg-primary p-2 m-2 border-none cursor-pointer rounded-[7.5px] text-sm"
+            onClick={() => onCancel()}
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </>
