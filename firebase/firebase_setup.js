21a7dcc89705746c82bc8079cdfc098a64bedcc5
Adithya Kashyap Pulipaka <adithya2205@gmail.com>
2023-04-16 21:10:16 -0400

    firebase setup and functionality with reducer

---
+++ firebase/firebase_setup.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
---
+++ reducers/todo.reducer.js
@@ -0,0 +1,32 @@
+export const TODO_OPS = {
+  INIT: "INIT",
+  ADD: "ADD",
+  DELETE: "DELETE",
+  EDIT: "EDIT",
+  COMPLETED: "COMPLETED",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case TODO_OPS.INIT:
+      return [...action.payload];
+    case TODO_OPS.ADD:
+      return [action.payload, ...state];
+    case TODO_OPS.DELETE:
+      const data = state.filter((todo) => {
+        return todo.id !== action.payload;
+      });
+      return data;
+    case TODO_OPS.COMPLETED: {
+      const data = state.filter((todo) => {
+        if (todo.id === action.payload) {
+          todo.completed = true;
+        }
+        return todo;
+      });
+      return data;
+    }
+  }
+}
+
+export default reducer;
